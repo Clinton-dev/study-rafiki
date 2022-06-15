7442490ca4fa1e0ccb3540fa9818663f5bb39cfc
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-15 23:23:21 +0300

    Add create and update form for room model

--- base/admin.py
+++ base/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Room)
+admin.site.register(models.Message)
+admin.site.register(models.Topic)
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 def home(request):
     rooms = Room.objects.all()
@@ -12,3 +13,33 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     return render(request,"base/room.html", {'room':room})
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'base/room_form.html', context )
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form =  RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {
+        'form': form
+    }
+    return render(request, 'base/room_form.html', context)
