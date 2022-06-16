1d7508f8fcdc7250abf487ba14b66d52f41e1616
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-16 07:48:31 +0300

    Add filter functionality based on topics

--- base/views.py
+++ base/views.py
@@ -4,7 +4,8 @@ from .models import Room,Topic
 from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__contains=q)
     topics = Topic.objects.all()
 
     context = {
