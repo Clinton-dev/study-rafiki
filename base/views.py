284c09a02aadacefb89bf3e576efb8200dbca11f
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-16 09:08:43 +0300

    Make the search form dynamic

--- base/views.py
+++ base/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q #allow chaining of queries
 from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__contains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(host__username__icontains=q) |
+        Q(description__icontains=q) |
+        Q(name__icontains=q)
+    )
     topics = Topic.objects.all()
 
     context = {
